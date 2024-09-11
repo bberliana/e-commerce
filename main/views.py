2c35ce9c53ce6f23efcadd24de4039949a80b46f
bberliana <raniaberliana96@gmail.com>
2024-09-11 10:25:54 +0700

    finish READ ME

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'nama' : 'Rania Berliana',
+        'kelas' : 'PBP B',
         'name' : 'Jodie Handbag',
         'price': '30000000',
         'description': 'A beautiful handbag made from the finest leather',
