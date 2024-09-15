c0ed35130b35d859d63d329a217ce533460bf88a
bberliana <raniaberliana96@gmail.com>
2024-09-16 00:31:06 +0700

    finish tutorial 2

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*f8h6&glb2fxh%8%qr_cp-t2v53#u7+ao&mtz-$($aeg8ut@c@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rania-berliana-ecommerce.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://rania-berliana-ecommerce.pbp.cs.ui.ac.id, https://rania-berliana-ecommerce.pbp.cs.ui.ac.id/"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'e_commerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
